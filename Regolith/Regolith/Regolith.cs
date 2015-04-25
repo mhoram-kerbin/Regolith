fbf8fab02ae92fc2da7b10732c6238996b2c13e9
Bob Palmer <rbpalmer2222@gmail.com>
2015-04-24 23:04:18 -0400

    Clearing out Regolith for 1.0

---
+++ Regolith/Regolith/Regolith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Regolith
+{
+    public class Regolith
+    {
+        //This is a dummy file - Regolith will, over time, transform into a collection of sub-classes off of 
+        //the stock resource system.
+    }
+}
--- Regolith/Regolith/Scenario/BiomeLockData.cs
+++
@@ -1,8 +0,0 @@
-namespace Regolith.Scenario
-{
-    public class BiomeLockData
-    {
-        public int PlanetId { get; set; }
-        public string BiomeName { get; set; }
-    }
-}
